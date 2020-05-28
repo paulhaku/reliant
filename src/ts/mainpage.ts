1367c5b9d0b53fc926dbd1287930da3c2232fda1
Haku <tenthdescent@gmail.com>
2020-05-28 10:14:40 -0400

    possibly finished initial design

--- src/ts/mainpage.ts
+++ src/ts/mainpage.ts
@@ -3,10 +3,11 @@ document.head.innerHTML = '<title>Reliant</title><meta charset="utf-8">';
 const pageContent = document.createElement('div');
 pageContent.id = "content";
 pageContent.innerHTML = `
-<div id="top">
+<div id="group1">
     <!-- Status -->
     <div id="status-container">
     <span id="status-header" class="header">Status</span>
+    <br>
     <br>
     <span id="status" class="information">N/A</span>
     </div>
@@ -23,10 +24,14 @@ pageContent.innerHTML = `
     </div>
 </div>
 
-<div id="middle">
+<div id="group2">
     <!-- Endorsing -->
     <div id="endorse-container">
         <span id="endorse-header" class="header">Endorse</span>
+        <br>
+        <input type="button" id="refresh-endorse" value="Refresh">
+        <br>
+        <br>
         <div class="information">
             <ul>
                 <li><a href="/nation=nation1">Nation 1</a></li>
@@ -39,13 +44,15 @@ pageContent.innerHTML = `
     <!-- Dossier -->
     <div id="dossier-container">
         <span id="dossier-header" class="header">Dossier</span>
+        <br>
+        <input type="button" id="refresh-dossier" value="Refresh">
         <div>
             <label for="raider-jp">Raider Jump Point</label>
             <input type="text" id="raider-jp">
             <input type="button" id="set-raider-jp" value="Set">
         </div>
         <div class="information">
-            <ul>
+            <ul id="nations-to-dossier">
                 <li><a href="/nation=nation1">Nation 1</a></li>
                 <li><a href="/nation=nation2">Nation 2</a></li>
                 <li><a href="/nation=nation3">Nation 3</a></li>
@@ -54,12 +61,25 @@ pageContent.innerHTML = `
     </div>
 </div>
 
-<div id="bottom">
+<div id="group3">
     <!-- Chasing -->
     <div id="chasing-container">
         <span id="chasing-header" class="header">Chasing</span>
         <br>
         <input type="button" id="chasing-button" value="Refresh">
+    </div>
+</div>
+
+<div id="group4">
+    <div id="switchers-container">
+        <span id="switchers-container" class="header">Switchers Left</span>
+        <div class="information">
+            <ul id="switchers">
+            <li>scent of night</li>
+            <li>rising walls</li>
+            <li>disintegrating</li>
+            </ul>
+        </div>
     </div>
 </div>
 `;

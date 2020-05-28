9eed3ae7ab92adba880f3cb50d2e164be985bef9
Haku <tenthdescent@gmail.com>
2020-05-27 21:08:42 -0400

    added url parameter grabber and pretty to helpers

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,3 +6,19 @@ function canonicalize(str: string): string
 {
     return str.trim().toLowerCase().replace(/ /g, '_');
 }
+
+function getUrlParameters(url: string): object
+{
+    const reg: RegExp = new RegExp("\/([A-Za-z0-9-]+?)=([A-Za-z0-9_.+]+)", "g");
+    let params: object = {};
+    let match: Match;
+    while ((match = reg.exec(url)) !== null)
+        params[match[1]] = match[2];
+    return params;
+}
+
+function pretty(str: string): string
+{
+    return str.replace(/_/g, ' ').replace(/\w+\s*/g, (txt: string) => txt.charAt(0).toUpperCase()
+    + txt.substr(1).toLowerCase());
+}

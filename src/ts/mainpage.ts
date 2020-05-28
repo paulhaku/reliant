a5180b5deaf0f854160a789943347208600bb9b5
Haku <tenthdescent@gmail.com>
2020-05-28 08:00:05 -0400

    removed title from main page and adjusted css a bit

--- src/ts/mainpage.ts
+++ src/ts/mainpage.ts
@@ -1,3 +1,20 @@
-/**
- * Created by J on 2020-05-27.
- */
+document.head.innerHTML = '<title>Reliant</title><meta charset="utf-8">';
+
+const pageContent = document.createElement('div');
+pageContent.id = "content";
+pageContent.innerHTML = `
+<!-- Status -->
+<div id="status-container">
+<span id="status-header" class="header">Status</span>
+<br>
+<span id="status" class="information">N/A</span>
+</div>
+
+<!-- Current WA Nation -->
+<div id="current-wa-nation-container">
+<span id="current-wa-nation-header" class="header">Current WA Nation</span>
+<br>
+<span id="current-wa-nation" class="information">N/A</span>
+</div>
+`;
+document.body.appendChild(pageContent);

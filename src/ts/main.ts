ed2af5da3fa1a14797fe151b385359a0c037dab3
Haku <tenthdescent@gmail.com>
2020-05-27 22:42:53 -0400

    switched all double quotes to single quotes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -9,7 +9,7 @@ function canonicalize(str: string): string
 
 function getUrlParameters(url: string): object
 {
-    const reg: RegExp = new RegExp("\/([A-Za-z0-9-]+?)=([A-Za-z0-9_.+]+)", "g");
+    const reg: RegExp = new RegExp('\/([A-Za-z0-9-]+?)=([A-Za-z0-9_.+]+)', 'g');
     let params: object = {};
     let match: Match;
     while ((match = reg.exec(url)) !== null)
@@ -29,7 +29,7 @@ function pretty(str: string): string
 
 function keyPress(e: KeyboardEvent): void
 {
-    const textboxSelected: HTMLElement = document.querySelector("input:focus, textarea:focus");
+    const textboxSelected: HTMLElement = document.querySelector('input:focus, textarea:focus');
     if (e.ctrlKey || e.altKey || e.shiftKey)
         return;
     else if (textboxSelected)
@@ -37,7 +37,7 @@ function keyPress(e: KeyboardEvent): void
     const pressedKey: string = e.key.toUpperCase();
 }
 
-document.addEventListener("keyup", keyPress);
+document.addEventListener('keyup', keyPress);
 
 /*
  * Keybind Functionality
@@ -45,13 +45,47 @@ document.addEventListener("keyup", keyPress);
 
 let keys: object = {};
 
-chrome.storage.local.get("movekey", (result) => {
+chrome.storage.local.get('movekey', (result) =>
+{
     const moveKey = result.movekey || 'X';
-    keys[moveKey] = () => {
+    keys[moveKey] = () =>
+    {
         const moveButton = document.querySelector('button[name=move_region]');
         if (moveButton)
             moveButton.click();
-        else if (urlParameters["page"] === "reports")
-            document.querySelector("li > a:nth-of-type(3)").click();
+        else if (urlParameters['page'] === 'reports')
+            document.querySelector('li > a:nth-of-type(3)').click();
     };
 });
+
+/*
+ * Miscellaneous
+ */
+
+const settingsParent = document.querySelector('.belspacer.belspacermain');
+if (settingsParent) {
+    // Settings Button
+    let settingsDiv = document.createElement('div');
+    settingsDiv.setAttribute('class', 'bel');
+    let settingsButton = document.createElement('button');
+    settingsButton.setAttribute('id', 'reliant-settings');
+    settingsButton.setAttribute('class', 'button');
+    settingsButton.innerHTML = 'Reliant Settings';
+    settingsDiv.appendChild(settingsButton);
+
+    // Main Reliant Button
+    let reliantButton = document.createElement('button');
+    reliantButton.setAttribute('id', 'reliant-main');
+    reliantButton.setAttribute('class', 'button');
+    reliantButton.innerHTML = 'Reliant';
+    settingsDiv.appendChild(reliantButton);
+    settingsParent.insertBefore(settingsDiv, settingsParent.firstChild);
+    document.querySelector('#reliant-settings').addEventListener('click', (e: MouseEvent) =>
+    {
+        window.location.href = '/page=blank/reliant_settings';
+    });
+    document.querySelector('#reliant-main').addEventListener('click', (e: MouseEvent) =>
+    {
+        window.location.href = '/template-overall=none/page=blank/reliant';
+    });
+}

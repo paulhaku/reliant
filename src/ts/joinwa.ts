5247b76b35b367503416e08d85c0840cab7a82e2
Haku <tenthdescent@gmail.com>
2020-05-30 10:05:26 -0400

    fixed issue with switcher regex on join wa page

--- src/ts/joinwa.ts
+++ src/ts/joinwa.ts
@@ -15,7 +15,7 @@ function addSwitcher(switcher: string, appid: string): void
 }
 
 if (urlParameters['page'] === 'join_WA') {
-    const switcherRegex: RegExp = new RegExp(`nation=([A-za-z0-9_]+?)&appid=([0-9]+)`, 'g');
+    const switcherRegex: RegExp = new RegExp(`nation=([A-Za-z0-9_]+?)&appid=([0-9]+)`, 'g');
     const match = switcherRegex.exec(document.URL);
     addSwitcher(match[1], match[2]);
 }

db33f7d14db279e7d639845f4db2835b7c31c101
Haku <tenthdescent@gmail.com>
2020-06-01 21:50:31 -0400

    added escape characters to ajax2 regex

--- src/ts/ajax2.ts
+++ src/ts/ajax2.ts
@@ -1,6 +1,6 @@
 if (urlParameters['page'] === 'ajax2') {
     let a = document.querySelectorAll('a');
-    let itemRegex: RegExp = new RegExp('page=ajax2/.+?/((?:nation|region)=.+)');
+    let itemRegex: RegExp = new RegExp('page=ajax2\/.+?\/((?:nation|region)=.+)');
     for (let i = 0; i != a.length; i++) {
         const match = itemRegex.exec(a[i].href);
         a[i].href = `/template-overall=none/${match[1]}`;

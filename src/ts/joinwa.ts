735e126dc7873bf1909f9975a212a70e7dd28c74
Haku <tenthdescent@gmail.com>
2020-06-28 13:00:05 -0400

    join wa file is now in iife

--- src/ts/joinwa.ts
+++ src/ts/joinwa.ts
@@ -1,23 +1,26 @@
-function addSwitcher(newSwitcher: Switcher): void
+(() =>
 {
-    chrome.storage.local.get('switchers', (result) =>
+    function addSwitcher(newSwitcher: Switcher): void
     {
-        let switchers: Switcher[] = [];
-        if (typeof result.switchers !== 'undefined')
-            switchers = result.switchers;
-        if (!(switchers.some(switcher => switcher.name === newSwitcher.name))) {
-            switchers.push(newSwitcher);
-            chrome.storage.local.set({'switchers': switchers});
-        }
-    });
-}
+        chrome.storage.local.get('switchers', (result) =>
+        {
+            let switchers: Switcher[] = [];
+            if (typeof result.switchers !== 'undefined')
+                switchers = result.switchers;
+            if (!(switchers.some(switcher => switcher.name === newSwitcher.name))) {
+                switchers.push(newSwitcher);
+                chrome.storage.local.set({'switchers': switchers});
+            }
+        });
+    }
 
-if (urlParameters['page'] === 'join_WA') {
-    const switcherRegex: RegExp = new RegExp(`nation=([A-Za-z0-9_-]+?)&appid=([0-9]+)`, 'g');
-    const match = switcherRegex.exec(document.URL);
-    const newSwitcher: Switcher = {
-        name: match[1],
-        appid: match[2]
-    };
-    addSwitcher(newSwitcher);
-}
+    if (urlParameters['page'] === 'join_WA') {
+        const switcherRegex: RegExp = new RegExp(`nation=([A-Za-z0-9_-]+?)&appid=([0-9]+)`, 'g');
+        const match = switcherRegex.exec(document.URL);
+        const newSwitcher: Switcher = {
+            name: match[1],
+            appid: match[2]
+        };
+        addSwitcher(newSwitcher);
+    }
+})();

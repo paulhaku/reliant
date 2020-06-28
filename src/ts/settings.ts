31333cde8be1994f32230b728926b310e0e01399
Haku <tenthdescent@gmail.com>
2020-06-27 20:14:13 -0400

    current keys are now viewable

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -53,82 +53,97 @@ pageContent.innerHTML = `
 <p>
 <label for="movekey">Move Key</label>
 <input type="radio" name="key-to-change" value="movekey">
+Current:
+<b id="currentmovekey"></b>
 </p>
 <p>
 <label for="jpkey">Jump Point Key</label>
 <input type="radio" name="key-to-change" value="jpkey">
+Current:
+<b id="currentjpkey"></b>
 </p>
 <p>
 <label for="refreshkey">Refresh Key</label>
 <input type="radio" name="key-to-change" value="refreshkey">
+Current:
+<b id="currentrefreshkey"></b>
 </p>
 <p>
-<label for="reportskey">Main Page Key</label>
+<label for="mainpagekey">Main Page Key</label>
 <input type="radio" name="key-to-change" value="mainpagekey">
+Current:
+<b id="currentmainpagekey"></b>
 </p>
 <p>
 <label for="resignkey">Resign Key</label>
 <input type="radio" name="key-to-change" value="resignkey">
+Current:
+<b id="currentresignkey"></b>
 </p>
 <p>
 <label for="dossierkey">View/Clear Dossier Key</label>
 <input type="radio" name="key-to-change" value="dossierkey">
+Current:
+<b id="currentdossierkey"></b>
 </p>
 <p>
 <label for="dossiernationkey">Dossier Nation Key</label>
 <input type="radio" name="key-to-change" value="dossiernationkey">
+Current:
+<b id="currentdossiernationkey"></b>
 </p>
 <p>
 <label for="endorsekey">Endorse Key</label>
 <input type="radio" name="key-to-change" value="endorsekey">
+Current:
+<b id="currentendorsekey"></b>
 </p>
 <p>
 <label for="gcrkey">GCR Updating Key</label>
 <input type="radio" name="key-to-change" value="gcrkey">
-</p>
-<p>
-<label for="regionajax2key">View Region Ajax2</label>
-<input type="radio" name="key-to-change" value="regionajax2key">
+Current:
+<b id="currentgcrkey"></b>
 </p>
 <p>
 <label for="viewregionkey">View Region</label>
 <input type="radio" name="key-to-change" value="viewregionkey">
+Current:
+<b id="currentviewregionkey"></b>
 </p>
 <p>
 <label for="rokey">RO Key</label>
 <input type="radio" name="key-to-change" value="rokey">
-</p>
-<p>
-<label for="banjectkey">Banject Key</label>
-<input type="radio" name="key-to-change" value="banjectkey">
+Not Implemented
 </p>
 <p>
 <label for="worldactivitykey">World Activity Key</label>
 <input type="radio" name="key-to-change" value="worldactivitykey">
+Current:
+<b id="currentworldactivitykey"></b>
 </p>
 <p>
 <label for="didiupdatekey">Did I Update? Key</label>
 <input type="radio" name="key-to-change" value="didiupdatekey">
+Current:
+<b id="currentdidiupdatekey"></b>
 </p>
 <p>
-<label for="delegatekey">Endorse Delegate/Endorse Vinny Key</label>
+<label for="delegatekey">Endorse Delegate Key</label>
 <input type="radio" name="key-to-change" value="delegatekey">
-</p>
-<p>
-<label for="toggletemplatekey">Toggle Template Key</label>
-<input type="radio" name="key-to-change" value="toggletemplatekey">
+Current:
+<b id="currentdelegatekey"></b>
 </p>
 <p>
 <label for="prepkey">Prep Key</label>
 <input type="radio" name="key-to-change" value="prepkey">
-</p>
-<p>
-<label for="detagkey">Detag Key</label>
-<input type="radio" name="key-to-change" value="detagkey">
+Current:
+<b id="currentprepkey"></b>
 </p>
 <p>
 <label>Settings Key</label>
 <input type="radio" name="key-to-change" value="settingskey">
+Current:
+<b id="currentsettingskey"></b>
 </p>
 <input type="text" id="new-key" maxlength="1">
 <input class="button" type="button" id="set-key" value="Set">
@@ -221,3 +236,60 @@ chrome.storage.local.get('prepswitchers', (result) =>
     for (let i = 0; i != prepSwitchers.length; i++)
         currentSwitcherSet.innerHTML += `${prepSwitchers[i]}<br>`;
 });
+
+/*
+ * Initialization
+ */
+
+(() =>
+{
+    async function getCurrentKey(key: string): Promise<string>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            chrome.storage.local.get(key, (result) =>
+            {
+                resolve(result[key]);
+            });
+        });
+    }
+
+    async function displayCurrentKeys()
+    {
+        const currentKeys = await Promise.all([
+            getCurrentKey('movekey'),
+            getCurrentKey('jpkey'),
+            getCurrentKey('refreshkey'),
+            getCurrentKey('mainpagekey'),
+            getCurrentKey('resignkey'),
+            getCurrentKey('dossierkey'),
+            getCurrentKey('dossiernationkey'),
+            getCurrentKey('endorsekey'),
+            getCurrentKey('gcrkey'),
+            getCurrentKey('viewregionkey'),
+            getCurrentKey('worldactivitykey'),
+            getCurrentKey('didiupdatekey'),
+            getCurrentKey('delegatekey'),
+            getCurrentKey('prepkey'),
+            getCurrentKey('settingskey')
+        ]);
+
+        document.querySelector('#currentmovekey').innerHTML = currentKeys[0] || 'X';
+        document.querySelector('#currentrefreshkey').innerHTML = currentKeys[1] || 'C';
+        document.querySelector('#currentjpkey').innerHTML = currentKeys[2] || 'V';
+        document.querySelector('#currentmainpagekey').innerHTML = currentKeys[3] || 'Space';
+        document.querySelector('#currentresignkey').innerHTML = currentKeys[4] || "'";
+        document.querySelector('#currentdossierkey').innerHTML = currentKeys[5] || 'M';
+        document.querySelector('#currentdossiernationkey').innerHTML = currentKeys[6] || 'N';
+        document.querySelector('#currentendorsekey').innerHTML = currentKeys[7] || 'Z';
+        document.querySelector('#currentgcrkey').innerHTML = currentKeys[8] || 'G';
+        document.querySelector('#currentviewregionkey').innerHTML = currentKeys[9] || 'D';
+        document.querySelector('#currentworldactivitykey').innerHTML = currentKeys[10] || 'F';
+        document.querySelector('#currentdidiupdatekey').innerHTML = currentKeys[11] || 'U';
+        document.querySelector('#currentdelegatekey').innerHTML = currentKeys[12] || 'A';
+        document.querySelector('#currentprepkey').innerHTML = currentKeys[13] || 'P';
+        document.querySelector('#currentsettingskey').innerHTML = currentKeys[14] || '0';
+    }
+
+    displayCurrentKeys();
+})();

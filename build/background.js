352d4f346dba22605f180f40471c7713bfb29ae1
Haku <tenthdescent@gmail.com>
2024-06-05 23:23:10 -0400

    Delete old background file

--- build/background.js
+++
@@ -1,8 +0,0 @@
-chrome.webRequest.onBeforeRedirect.addListener(details => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { redirecturl: details.redirectUrl, message: "redirecturl" }, function (response) {
-        });
-    });
-}, {
-    urls: ["<all_urls>"]
-});

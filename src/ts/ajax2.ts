1b9ddb3ed60d0433e5375583a85dcc369134f7d6
Haku <tenthdescent@gmail.com>
2024-06-08 17:00:30 -0400

    Fix SVG image sizes on ajax2 pages

--- src/ts/ajax2.ts
+++ src/ts/ajax2.ts
@@ -1,8 +1,37 @@
-if (urlParameters['page'] === 'ajax2') {
-    let a = document.querySelectorAll('a');
-    let itemRegex: RegExp = new RegExp('page=ajax2\/.+?\/((?:nation|region)=.+)');
-    for (let i = 0; i != a.length; i++) {
-        const match = itemRegex.exec(a[i].href);
-        a[i].href = `/template-overall=none/${match[1]}`;
+(() => {
+    const getListItems = (): Element[] => Array.from(document.querySelectorAll('li'));
+    const getListItemImages = (item: Element): NodeListOf<HTMLImageElement> => item.querySelectorAll('img');
+    const resizeImage = (image: HTMLImageElement): void => {
+        if (image) {
+            image.style.width = '20px';
+            image.style.height = '20px';
+        }
+    };
+
+    const updateLinkHref = (link: HTMLAnchorElement, regex: RegExp): HTMLAnchorElement => {
+        const match = regex.exec(link.href);
+        if (match) {
+            link.href = `/template-overall=none/${match[1]}`;
+        }
+        return link;
+    };
+
+    const processLinks = (): void => {
+        const links = Array.from(document.querySelectorAll('a'));
+        const itemRegex = /page=ajax2\/.+?\/((?:nation|region)=.+)/;
+        links.forEach(link => updateLinkHref(link as HTMLAnchorElement, itemRegex));
+    };
+
+    const processImages = (): void => {
+        const items = getListItems();
+        items.forEach(item => {
+            const images = getListItemImages(item);
+            images.forEach(resizeImage);
+        });
+    };
+
+    if (urlParameters['page'] === 'ajax2') {
+        processLinks();
+        processImages();
     }
-}
+})();
